340b21abaa7e896c2b2e32c0bf98ec42917c5dd4
arthurrodrigo2 <arthurrodrigo2@gmail.com>
2024-09-23 19:59:36 -0300

    Versão 1.0

---
+++ app.py
@@ -0,0 +1,7 @@
+nome = input('informe seu nome: ')
+idade = int(input('informe sua idade: '))
+
+#verifica a idade do usuario
+print(f"{nome}é maior de idade,"if idade >= 18 else f"{nome} é menor de idade")
+ 
+
